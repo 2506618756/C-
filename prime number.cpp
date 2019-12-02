62478e0b51ff0ff6188fd1752af3caf653c0ac75
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-02 22:03:21 +0800

    Update prime number.cpp
    素筛

--- prime number.cpp
+++ prime number.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <string.h>
-#define MAX 1000
+#define MAX 10000
 using namespace std;
 int prime[MAX];
-int k,num[MAX];
-int temp[MAX];
-int visted[MAX];
+int k,num[21];
+int temp[21];
+int visted[21];
 int cnt=0;
 void getprime()
 {
-	prime[0] = prime[1] = 1;
-	for( int i=2; i<=MAX; i++ )
+	for( int i=2; i<MAX; i++ )
 	{
 		if( prime[i]==0 )
 		{
@@ -28,23 +27,19 @@ int main()
 	int first=1;
 	int cnt=0;
 	int n;
-//	cin>>n;
-//	for( int i=2; i<MAX; i++ )
-//	{
-//		if(n==0)
-//			break;
-//		if( prime[i]==0 )
-//		{
-//			if( i-first==2 )
-//				cnt++;
-//			first = i;
-//			n--;
-//		}
-//	}
-	for( int i=1; i<=MAX; i++ )
+	cin>>n;
+	for( int i=2; i<MAX; i++ )
 	{
+		if(n==0)
+			break;
 		if( prime[i]==0 )
-			cout<<i<<endl;
+		{
+			if( i-first==2 )
+				cnt++;
+			first = i;
+			n--;
+		}
 	}
+	cout<<cnt<<endl;
 	return 0;
 }
