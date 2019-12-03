32517efab64903c9c17d43e2690dacf1136fafb1
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-03 21:11:50 +0800

    1203
    更新

--- PTA--朋友圈.cpp
+++ PTA--朋友圈.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int s[30001];
+int hight[30001];
 int root[30001]={0};
 int find_set( int x ){
 	return x==s[x]?x:find_set(s[x]);
@@ -9,13 +10,23 @@ int find_set( int x ){
 void union_set( int x,int y ){
 	x = find_set(x);
 	y = find_set(y);
-	if(x!=y) s[x] = y;
+	if( hight[x]==hight[y] ){
+		hight[x] = hight[x] + 1;
+		s[y] = x;
+	}
+	else{
+		if( hight[x]<hight[y] ) s[x] = y;
+		else s[y] = x;
+	}
 }
 int main(){
 	int n,m,k,x,y;
 	cin>>n>>m;
 	for( int i=1; i<=n; i++ )
-		s[i] = i;		
+	{
+		s[i] = i;
+		hight[i] = 0;
+	}
 	for( int i=0; i<m; i++ ){
 		cin>>k>>x;
 		for( int j=1; j<k; j++ ){
