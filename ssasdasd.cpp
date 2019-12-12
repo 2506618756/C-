2f547903ec8bfefba590d58f9f1dab0425718b4f
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-12 22:24:49 +0800

    1212

--- ssasdasd.cpp
+++ ssasdasd.cpp
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
 	int n;
-	while( ~scanf("%d",&n ) )
-		printf("%d\n",n%10);
+	scanf("%d",&n);
+	int ans = 0;
+	while(n)
+	{
+		ans = ans * 10 + n % 10;
+		n /= 10;
+	}while(n);
+	do
+	{
+		printf("%d ",ans%10);
+		ans /= 10;
+	}while(ans);
 	return 0;
 }
-//
+
