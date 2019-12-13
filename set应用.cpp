846521496e368e217a9c3c825382306e7ec314f0
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-13 22:06:59 +0800

    1213

--- set应用.cpp
+++ set应用.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <cstdio>
+#include <set>
 using namespace std;
 int main()
 {
-	
+	ios::sync_with_stdio(false);
+	int n,x,flag=0;
+	cin>>n;
+	set<int> s;
+	s.clear();
+	pair<set<int>::iterator,bool> ans;
+	for( int i=0; i<n; i++ )
+	{
+		cin>>x;
+		ans = s.insert(x);
+		if( ans.second==false )
+			flag = 1;
+	}
+	if(flag) printf("YES\n");
+	else printf("NO\n");
 	return 0;
 }
