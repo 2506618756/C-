8aee2fa1134a4c2e27535f74df890d86fee368d8
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-20 22:19:28 +0800

    Update 链表 建立学生信息链表.cpp

--- 链表 建立学生信息链表.cpp
+++ 链表 建立学生信息链表.cpp
@@ -22,3 +22,51 @@ int main()
 
     return 0;
 }
+#include <string.h>
+struct stud_node *createlist()
+{
+int number,score;
+char name[100];
+struct stud_node *p=NULL,*head=NULL,*tail=NULL;
+scanf("%d",&number);
+while(number!=0)
+{
+scanf("%s %d",name,&score);
+p=(struct stud_node *)malloc(sizeof(struct stud_node));
+p->num=number;
+p->score=score;
+strcpy(p->name,name);
+p->next=NULL;
+if(head==NULL) head=p;
+else tail->next=p;
+tail=p;
+scanf("%d",&number);
+}
+return head;
+}
+struct stud_node *deletelist( struct stud_node *head, int min_score )
+{
+struct stud_node *ptr=NULL,*ptr1=NULL,*ptr2=NULL;
+if(head==NULL) return NULL;
+for(ptr=head;ptr!=NULL;ptr=ptr->next)
+{
+if(ptr->score<min_score)
+{
+if(ptr==head)
+{
+head=head->next;
+free(ptr);
+}
+else
+{
+ptr1->next=ptr->next;
+free(ptr);
+}
+}
+else
+{
+ptr1=ptr;
+}
+}
+return head;
+}
