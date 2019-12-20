e9e45b535ae879dfeef32ea639083290ac35b520
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-20 22:17:40 +0800

    Create 链表 建立学生信息链表.cpp

---
+++ 链表 建立学生信息链表.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct stud_node {
+     int    num;
+     char   name[20];
+     int    score;
+     struct stud_node *next;
+};
+struct stud_node *head, *tail;
+
+void input();
+
+int main()
+{
+    struct stud_node *p;
+	
+    head = tail = NULL;
+    input();
+    for ( p = head; p != NULL; p = p->next )
+        printf("%d %s %d\n", p->num, p->name, p->score);
+
+    return 0;
+}
