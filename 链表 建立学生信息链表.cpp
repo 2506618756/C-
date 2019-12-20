58ba3d9dd07d5ba9b48f61ff62fa8de35b2af81b
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-20 22:18:58 +0800

    Update 链表 建立学生信息链表.cpp

--- 链表 建立学生信息链表.cpp
+++ 链表 建立学生信息链表.cpp
@@ -1,51 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 struct stud_node {
      int    num;
      char   name[20];
      int    score;
      struct stud_node *next;
 };
-struct stud_node *head, *tail;
 
-void input();
-
+struct stud_node *createlist();
+struct stud_node *deletelist( struct stud_node *head, int min_score );
 int main()
 {
-    struct stud_node *p;
-	
-    head = tail = NULL;
-    input();
+    int min_score;
+    struct stud_node *p, *head = NULL;
+
+    head = createlist();
+    scanf("%d", &min_score);
+    head = deletelist(head, min_score);
     for ( p = head; p != NULL; p = p->next )
         printf("%d %s %d\n", p->num, p->name, p->score);
 
     return 0;
-}
-void input()
-{
-	int x;
-	struct stud_node *p;
-	while( ~scanf("%d",&x) && x!=0 )
-	{
-		p = (struct stud_node *)malloc(sizeof(struct stud_node));
-		p->num = x;
-		scanf("%s",p->name);
-		scanf("%d",&p->score);
-		if( head==NULL )
-		{
-			head = p;
-		}
-		else
-		{
-			tail = head;
-			while(tail->next!=NULL)
-			{
-				tail = tail->next;
-			}
-			tail->next = p;
-		}
-		p->next = NULL;
-	}
 }
