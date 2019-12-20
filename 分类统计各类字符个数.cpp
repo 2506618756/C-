55692b23c6563b0cb59a978c9d9725af5f58768d
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-20 22:16:37 +0800

    Update 分类统计各类字符个数.cpp

--- 分类统计各类字符个数.cpp
+++ 分类统计各类字符个数.cpp
@@ -13,3 +13,21 @@ int main()
 
     return 0;
 }
+void StringCount( char *s )
+{
+    int a=0,b=0,c=0,d=0,e=0;
+    for( int i=0; s[i]!='\0'; i++ )
+    {
+        if( s[i]>='A' && s[i]<='Z' )
+            a++;
+        else if( s[i]>='a' && s[i]<='z' )
+            b++;
+        else if( s[i]==' ' )
+            c++;
+        else if( s[i]>='0' && s[i]<='9' )
+            d++;
+        else
+            e++;
+    }
+    printf("%d %d %d %d %d\n",a,b,c,d,e);
+}
