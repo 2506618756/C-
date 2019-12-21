379aada83b489b8e87a03d1fccd8f36aff500fc2
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-21 19:04:30 +0800

    1221

--- 交换二叉树中每个结点的左孩子和右孩子.cpp
+++ 交换二叉树中每个结点的左孩子和右孩子.cpp
@@ -2,3 +2,61 @@
 #include <stdlib.h>
 #include <iostream>
 using namespace std;
+typedef char ElementType;
+typedef struct TNode *Position;
+typedef Position BinTree;
+struct TNode{
+    ElementType Data;
+    BinTree Left;
+    BinTree Right;
+};
+string str;
+int len,idx;
+
+void CreatBinTree(BinTree &BST)
+{
+	if(str[idx]=='#'||idx==len) return;
+	BST  = (BinTree)malloc(sizeof(struct TNode));
+	BST->Data = str[idx];
+	BST->Left = BST->Right = NULL;
+	idx++;
+	CreatBinTree(BST->Left);
+	idx++;
+	CreatBinTree(BST->Right);
+	return ;
+}
+void exchange(BinTree &BST)
+{
+	if( !BST->Left && !BST->Right ) return ;
+	BinTree temp;
+	temp = BST->Left;
+	BST->Left = BST->Right;
+	BST->Right = temp;
+	if( BST->Left )
+		exchange(BST->Left);
+	if( BST->Right )
+		exchange(BST->Right);
+}
+void zxprint(BinTree BST)
+{
+	if(BST!=NULL)
+	{
+		zxprint(BST->Left);
+		cout<<BST->Data;
+		zxprint(BST->Right);
+	}
+}
+
+int main()
+{
+    BinTree BT=NULL;
+    cin>>str;
+    len = str.length();
+    idx = 0;
+	CreatBinTree(BT);
+    zxprint(BT);
+    cout<<endl;
+    exchange(BT);
+    zxprint(BT);
+    return 0;
+}
