a05346ad18ce5f2d01e47ac83bae44bf21b9a394
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-21 19:01:50 +0800

    1221

---
+++ 二叉树高度.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char ElementType;
+typedef struct TNode *Position;
+typedef Position BinTree;
+struct TNode{
+    ElementType Data;
+    BinTree Left;
+    BinTree Right;
+};
+
+BinTree CreatBinTree(); /* ÊµÏÖÏ¸½ÚºöÂÔ */
+int GetHeight( BinTree BT );
+
+int main()
+{
+    BinTree BT = CreatBinTree();
+    printf("%d\n", GetHeight(BT));
+    return 0;
+}
