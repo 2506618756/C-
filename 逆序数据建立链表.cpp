500be7d9c8e9a7c031bcc648524614298cceb902
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-21 19:03:09 +0800

    1221

--- 逆序数据建立链表.cpp
+++ 逆序数据建立链表.cpp
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+struct ListNode {
+    int data;
+    struct ListNode *next;
+};
+
+struct ListNode *createlist();
+
+int main()
+{
+    struct ListNode *p, *head = NULL;
+
+    head = createlist();
+    for ( p = head; p != NULL; p = p->next )
+        printf("%d ", p->data);
+    printf("\n");
+
+    return 0;
+}
