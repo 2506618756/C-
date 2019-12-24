a2fd2bd1596e43023c2a40455d992feb1d899a84
2506618756 <51432893+2506618756@users.noreply.github.com>
2019-12-24 12:18:41 +0800

    Update 看病优先队列.cpp

--- 看病优先队列.cpp
+++ 看病优先队列.cpp
@@ -1,3 +1,52 @@
 #include <iostream>
-#include <string.h>
+#include <cstdio>
+#include <queue>
+#include <cstring> 
+#include <string>
 using namespace std;
+struct node{
+    int num,id;
+    bool friend operator < ( node a,node b )
+    {
+        if( a.num!=b.num )
+            return a.num < b.num;
+        else
+            return a.id > b.id;
+    }
+};
+int main()
+{
+    ios::sync_with_stdio(false);
+    int n,num,k;
+    string s;
+    while( ~scanf("%d",&n) )
+    {
+        int cnt = 0;
+        node t;
+        priority_queue<node> q[4];
+        while(n--)
+        {
+            cin>>s;
+            if( s=="IN" )
+            {
+                cin>>k>>num;
+                t.id = ++cnt;
+                t.num = num;
+                q[k].push(t);
+            }
+            else
+            {
+                cin>>k;
+                if(!q[k].empty())
+                {
+                    t = q[k].top();
+                    q[k].pop();
+                    printf("%d\n",t.id);
+                }
+                else
+                    printf("EMPTY\n");
+            }
+        }
+    }
+    return 0;
+}
